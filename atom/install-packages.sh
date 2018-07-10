7f53da2a62215b67d6e94924e86f717d6595702a
Mario Uriarte <mario.uriarte@gmail.com>
2018-07-10 01:34:42 -0400

    Sound inicio añadido

--- atom/install-packages.sh
+++ atom/install-packages.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-apm install docblockr
+#apm install docblockr
 apm install emmet
 apm install file-icons
 apm install highlight-selected
 apm install linter
 apm install linter-php
-apm install merge-conflicts
+#apm install merge-conflicts
 apm install minimap
 apm install minimap-highlight-selected
 apm install ramda-syntax
-apm install fonts
+#apm install fonts
 apm install tree-view-git-status
 apm install atom-beautify
 apm install gruvbox-plus-syntax
-apm isntall dark-flat-ui
-
-
+apm install dark-flat-ui
+apm install linter-ui-default
+#apm install intentions
+#apm install busy-signal
