b61a3fae3b0d5013d1f7eee2eaf46679d93ded19
Mario Uriarte <mario.uriarte@gmail.com>
2023-03-17 15:41:57 -0400

    limpieza de archivos inecesarios y scripts inecesarios, manjaro_install.sh implementado

---
+++ system/install_jasper.sh
@@ -0,0 +1,35 @@
+pkgname=jaspersoftstudio
+_pkgname=JaspersoftStudio
+_binname="Jaspersoft Studio"
+pkgver=6.20.1
+pkgrel=1
+pkgdesc="Eclipse based Jasper Reports generator"
+arch=('x86_64')
+url="http://community.jaspersoft.com/project/jaspersoft-studio"
+license=('Eclipse')
+depends=('java-runtime' 'gtk2')
+source_x86_64=("https://sourceforge.net/projects/jasperstudio/files/JaspersoftStudio-${pkgver}/js-studiocomm_${pkgver}_linux_x86_64.tgz")
+md5sums_x86_64=('d77dec170c9f2fc28d0dbf9b9d348c81')
+
+package() {
+install -d -m 0755 ${pkgdir}/opt/${pkgname}
+cp -a ${srcdir}/js-studiocomm_${pkgver}/* ${pkgdir}/opt/${pkgname}
+install -d -m 0755 ${pkgdir}/usr/bin
+ln -sf "/opt/${pkgname}/${_binname}" ${pkgdir}/usr/bin/${pkgname}
+ln -sf "/opt/${pkgname}/${_binname}" ${pkgdir}/usr/bin/${pkgname}
+
+cat > ${_pkgname}.desktop << EoF
+[Desktop Entry]
+Version=${pkgver}
+Encoding=UTF-8
+Name=Jaspersoft Studio
+Comment=${pkgdesc}
+Exec=env GTK2_RC_FILES=/usr/share/themes/Raleigh/gtk-2.0/gtkrc "/opt/${pkgname}/${_binname}"
+Icon=/opt/${pkgname}/icon.xpm
+Terminal=false
+Type=Application
+Categories=Java;Development;
+EoF
+
+install -D -m 0644 ${srcdir}/${_pkgname}.desktop ${pkgdir}/usr/share/applications/${pkgname}.desktop
+}
---
+++ system/manjaro_install.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+# sudo pacman-mirrors --fasttrack
+# sudo pacman -Syyu --noconfirm
+
+# sudo pacman -S --noconfirm \
+#     yay \
+#     docker docker-compose \
+#     apache \
+#     mariadb \
+#     php php-apache \
+#     wget \
+#     samba \
+#     vim neovim \
+#     git \
+#     base-devel \
+#     xclip unzip unrar
+
+# sudo pacman -S --noconfirm \
+#     keepassxc
+    # gitg
+#     filezilla \
+#     community/intellij-idea-community-edition
+#     gimp \
+#     thunderbird \
+#     libreoffice \    
+#     vivaldi \
+#     opera \
+#     brave-browser \
+#     kate \
+#     krite \
+#     amarok \
+#     vlc \
+#     zsh \
+#     spotify-launcher \
+
+# yay -S --noconfirm ttf-ms-win10-auto
+yay -S --noconfirm \
+    aur/jaspersoftstudio
+    # aur/git-cola
+    # aur/eclipse-java \
+    # aur/mailspring \
+    # aur/slack-desktop \
+    # community/discord \
+    # visual-studio-code-bin \
+    # jdk11-openjdk \
+    # jdk17-openjdk \
+    # latte-dock-git \
+    # google-chrome \
+    # aur/phpstorm \
+    # multilib/steam-native-runtime
+    
+# confi
+# echo "> Set configurations..."
+# echo ""
+# sudo systemctl start docker.service
+# sudo systemctl enable docker.service
