0f89a59f0911633cf586690e4bb43707704849b8
Mario Uriarte <mario.uriarte@gmail.com>
2023-03-21 10:32:11 -0400

    install manajaro update

--- scripts/install_manjaro_apps.sh
+++ scripts/install_manjaro_apps.sh
@@ -1,59 +1,61 @@
 #!/bin/sh
 echo "Install manjaro Apps"
 
-# sudo pacman-mirrors --fasttrack
-# sudo pacman -Syyu --noconfirm
+sudo pacman-mirrors --fasttrack
+sudo pacman -Syyu --noconfirm
 
-# sudo pacman -S --noconfirm \
-#     yay \
-#     docker docker-compose \
-#     apache \
-#     mariadb \
-#     php php-apache \
-#     wget \
-#     samba \
-#     vim neovim \
-#     git \
-#     base-devel \
-#     xclip unzip unrar
+sudo pacman -S --noconfirm \
+    yay \
+    docker docker-compose \
+    apache \
+    mariadb \
+    php php-apache \
+    wget \
+    samba \
+    vim neovim \
+    git \
+    base-devel \
+    xclip unzip unrar \
+    openssh \
 
-# sudo pacman -S --noconfirm \
-#     keepassxc
-#     gitg
-#     filezilla \
-#     community/intellij-idea-community-edition
-#     gimp \
-#     thunderbird \
-#     libreoffice \
-#     vivaldi \
-#     opera \
-#     brave-browser \
-#     kate \
-#     krite \
-#     amarok \
-#     vlc \
-#     zsh \
-#     spotify-launcher \
 
-# yay -S --noconfirm ttf-ms-win10-auto
- yay -S --noconfirm \
-    aur/jaspersoftstudio
-    # aur/dry-bin \    
-    # aur/git-cola \
-    # aur/eclipse-java \
-    # aur/mailspring \
-    # aur/slack-desktop \
-    # community/discord \
-    # visual-studio-code-bin \
-    # jdk11-openjdk \
-    # jdk17-openjdk \
-    # latte-dock-git \
-    # google-chrome \
-    # aur/phpstorm \
-    # multilib/steam-native-runtime
+sudo pacman -S --noconfirm \
+    keepassxc \
+    gitg \
+    filezilla \
+    community/intellij-idea-community-edition \
+    gimp \
+    thunderbird \
+    libreoffice \
+    vivaldi \
+    opera \
+    brave-browser \
+    kate \
+    krite \
+    amarok \
+    vlc \
+    zsh \
+    spotify-launcher \
+
+yay -S --noconfirm ttf-ms-win10-auto
+yay -S --noconfirm \
+    aur/dry-bin \
+    aur/git-cola \
+    aur/eclipse-java \
+    aur/mailspring \
+    aur/slack-desktop \
+    community/discord \
+    visual-studio-code-bin \
+    jdk11-openjdk \
+    jdk17-openjdk \
+    latte-dock-git \
+    google-chrome \
+    aur/phpstorm \
+    multilib/steam-native-runtime \
+    aur/jaspersoftstudio \
 
 # confi
-# echo "> Set configurations..."
-# echo ""
-# sudo systemctl start docker.service
-# sudo systemctl enable docker.service
+echo "> Set configurations..."
+echo ""
+sudo systemctl start docker.service
+sudo systemctl enable docker.service
