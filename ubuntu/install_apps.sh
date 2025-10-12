eeb9db0ed045268411ac96dcdca86af241a715f8
Mario Uriarte <mario.uriarte@gmail.com>
2025-10-12 10:42:14 -0400

    update nvim ubuntu conf

---
+++ ubuntu/install_apps.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt install build-essential \
+    npm \
+    vim
--- ubuntu/install_nvim.sh
+++ ubuntu/install_nvim.sh
@@ -4,4 +4,4 @@ sudo apt install fonts-noto-color-emoji
 
 sudo add-apt-repository ppa:neovim-ppa/unstable -y
 sudo apt update
-sudo apt install make gcc ripgrep unzip git xclip neovim
+sudo apt install ripgrep unzip git xclip neovim
