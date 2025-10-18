e6169bdd676a9563d6af52a7fbf304e2ee1d4598
Mario Uriarte <mario.uriarte@gmail.com>
2025-10-17 23:09:32 -0400

    Change conf treesitter nvim

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,37 @@
+return {
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		--config = function()
+		--  require("nvim-treesitter.configs").setup({})
+		--end,
+	},
+}
+
+--local spec = {
+--  "nvim-treesitter/nvim-treesitter",
+--  build = ":TSUpdate",
+--  config = function()
+--    require("nvim-treesitter.configs").setup({
+--      ensure_installed = {
+--        -- "bash",
+--        "latex",
+--        "lua",
+--        "vim",
+--        "vimdoc",
+--      },
+--      sync_install = false,
+--      auto_install = false,
+--      highlight = {
+--        enable = true,
+--        disable = { "latex" },
+--      },
+--      indent = {
+--        enable = true,
+--        disable = { "latex" },
+--      },
+--    })
+--  end,
+--}
+--return spec
+--
--- nvim/setup.sh
+++ nvim/setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+rm ~/.config/nvim/lua/plugins/*
+
 cp nvim/init.lua ~/.config/nvim
 cp -r nvim/lua/ ~/.config/nvim
