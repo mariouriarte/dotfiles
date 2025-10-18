384dc90b9564e514c902d20ccb2f47f8eee5e959
Mario Uriarte <mario.uriarte@gmail.com>
2025-10-18 19:41:19 -0400

    Add alias group for bash

--- bash/setup.sh
+++ bash/setup.sh
@@ -2,19 +2,39 @@
 
 cp $HOME/.bashrc $HOME/.bashrc.bak
 
-sed -i '/^#--/d' $HOME/.zshrc
+sed -i -z 's/#<!--.*-->//g' $HOME/.zshrc
+sed -i -z 's/#<!--.*-->//g' $HOME/.bashrc
 
-multiline_string='
-#--custom-conf
+multiline_string='#<!-- custom-conf
+export VISUAL=vim
+export EDITOR=vim
+
 alias pfilteroc="ps auwwx"
 alias pfilter="ps auwwx | grep "
 alias start="systemctl start "
 alias stop="systemctl stop "
 alias ena="systemctl enable "
+
+alias ls="eza --icons=always"
+
+alias gco="git checkout"
 alias gs="git status"
-export VISUAL=vim
-export EDITOR=vim
-#--end-conf'
+alias ga="git add"
+alias ga.="git add ."
+alias gc="git commit"
+alias gcm="git commit -m"
+alias gca="git commit -a"
+alias gp="git push"
+alias gl="git pull"
+alias glg="git log --oneline --decorate --all --graph"
+
+alias v="nvim ."
+
+alias dc="docker compose"
+
+alias his="history | grep"
+
+# end-conf -->'
 
 cat <<EOF >>$HOME/.bashrc
 $multiline_string
@@ -26,3 +46,8 @@ $multiline_string
 EOF
 
 fi
+
+sed -i -e ':a' -e 'N' -e '$!ba' -e 's/\n\n/\n/g' $HOME/.zshrc
+sed -i -e ':a' -e 'N' -e '$!ba' -e 's/\n\n/\n/g' $HOME/.bashrc
+
+#[[ ! -f ~/.zshrc ]] || source ~/.zshrc
