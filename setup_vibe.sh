7ce6c6d2a41610e7e3b59d406cec59e8af6faf19
Mario Uriarte <mario.uriarte@gmail.com>
2025-10-23 23:33:04 -0400

    gs

--- setup_vibe.sh
+++ setup_vibe.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-git pull origin main
+#mkdir -p /var/log/dotfiles
+
+echo "$(date +'%Y-%m-%d %e:%M:%S') [INFO] Setup vibe Mario escrito" >>/tmp/setup_vibe.log
+echo "$(date +'%Y-%m-%d %e:%M:%S') [INFO] git stdout:" >>/tmp/setup_vibe.log
+
+cd /home/mario/dotfiles
+git pull origin main &>>/tmp/setup_vibe.log
 
 echo "Setup Vibe Mario..."
 source ./alacritty/setup.sh
