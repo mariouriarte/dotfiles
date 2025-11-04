3ff0dc0113c53df97fb4cd106a27b1bd93ac7f1d
Mario Uriarte <mario.uriarte@gmail.com>
2025-11-04 14:29:06 -0400

    Nvim update show files ignore

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -5,7 +5,7 @@ return {
 		opts = {
 			picker = {
 				--hidden = true, -- Shows hidden files (e.g., .git, .config)
-				ignored = true, -- Shows files ignored by .gitignore
+				--ignored = true, -- Shows files ignored by .gitignore
 			},
 		},
 	},
