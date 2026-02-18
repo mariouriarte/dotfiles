14403f5b16cb479c1080684ac3d07645fb038235
Mario Uriarte <mario.uriarte@gmail.com>
2026-02-17 23:20:12 -0400

    Alacritty conf envy 14

---
+++ alacritty/envy_conf.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sed -i 's/dimensions.columns = 140/dimensions.columns = 110/g' ~/.config/alacritty/alacritty.toml
+sed -i 's/dimensions.lines = 35/dimensions.lines = 25/g' ~/.config/alacritty/alacritty.toml
