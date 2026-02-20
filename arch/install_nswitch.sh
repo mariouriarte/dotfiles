acb197461d6067c4ede38331b36fe2af2e503dfa
Mario Uriarte <mario.uriarte@gmail.com>
2026-02-20 15:46:43 -0400

    Add controls in n switch scritp

--- arch/install_nswitch.sh
+++ arch/install_nswitch.sh
@@ -1,7 +1,22 @@
-$!/bin/bash
+#!/bin/bash
+set -euo pipefail
 
-yay -S --noconfirm libusb \
-    pyusb
+DEPENDENCIES=(
+    "libusb"
+    "python-pyusb"
+)
+REPO_URL="git@github.com:lunixoid/dbibackend.git"
+INSTALL_DIR="$HOME/Games/switch/dbibackend"
 
-mkdir -p ~/Games/switch
-git clone git@github.com:lunixoid/dbibackend.git ~/Games/switch/dbibackend
+echo "Installing dependencies..."
+yay -S --noconfirm "${DEPENDENCIES[@]}"
+
+mkdir -p "$(dirname "$INSTALL_DIR")"
+
+if [[ -d "$INSTALL_DIR" ]]; then
+    echo "Updating existing repository in $INSTALL_DIR..."
+    git -C "$INSTALL_DIR" pull
+else
+    echo "Cloning repository to $INSTALL_DIR..."
+    git clone "$REPO_URL" "$INSTALL_DIR"
+fi
