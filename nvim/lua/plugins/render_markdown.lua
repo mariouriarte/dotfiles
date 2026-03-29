f5655ceb1482bbf1f113efa2be886f6ae1a22a1e
Mario Uriarte <mario.uriarte@gmail.com>
2026-03-28 20:34:13 -0400

    read marker mini icon import fix

--- nvim/lua/plugins/render_markdown.lua
+++ nvim/lua/plugins/render_markdown.lua
@@ -1,33 +1,33 @@
 return {
-  {
-    "MeanderingProgrammer/render-markdown.nvim",
-    dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" }, -- Opcional pero recomendado
-    ft = { "markdown", "norg", "rmd", "org", "codecompanion" },
-    opts = {
-      -- Aquí puedes personalizar para que no sea tan "cargado"
-      heading = {
-        enabled = true,
-        sign = false, -- Quita los iconos de la columna lateral si te molestan
-        position = "overlay",
-      },
-      code = {
-        style = "language", -- Solo resalta el lenguaje, evita bordes pesados
-      },
-      checkbox = {
-        enabled = true,
-      },
-    },
-  },
-  {
-    "HakonHarnes/img-clip.nvim",
-    opts = {
-      filetypes = {
-        codecompanion = {
-          prompt_for_file_name = false,
-          template = "[Image]($FILE_PATH)",
-          use_absolute_path = true,
-        },
-      },
-    },
-  },
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.icons" }, -- Opcional pero recomendado
+		ft = { "markdown", "norg", "rmd", "org", "codecompanion" },
+		opts = {
+			-- Aquí puedes personalizar para que no sea tan "cargado"
+			heading = {
+				enabled = true,
+				sign = false, -- Quita los iconos de la columna lateral si te molestan
+				position = "overlay",
+			},
+			code = {
+				style = "language", -- Solo resalta el lenguaje, evita bordes pesados
+			},
+			checkbox = {
+				enabled = true,
+			},
+		},
+	},
+	{
+		"HakonHarnes/img-clip.nvim",
+		opts = {
+			filetypes = {
+				codecompanion = {
+					prompt_for_file_name = false,
+					template = "[Image]($FILE_PATH)",
+					use_absolute_path = true,
+				},
+			},
+		},
+	},
 }
