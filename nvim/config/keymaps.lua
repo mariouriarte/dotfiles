8fb4c96699a225b786c5a52f4b699c012367922b
Mario Uriarte <mario.uriarte@gmail.com>
2026-04-22 16:45:12 -0400

    Add force restart lsp nvim

---
+++ nvim/config/keymaps.lua
@@ -0,0 +1,23 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+-- Comentar linea o seleccion con control + shift + /
+vim.keymap.set("n", "<C-S-/>", "gcc", { remap = true })
+vim.keymap.set("v", "<C-S-/>", "gc", { remap = true })
+
+-- Cerrar buffer actual con Alt + w
+-- Usamos Snacks.bufdelete() porque es el estándar de LazyVim para no romper el layout
+vim.keymap.set("n", "<M-w>", function()
+  Snacks.bufdelete()
+end, { desc = "Cerrar Buffer (Alt+w)" })
+
+-- Terminal en split vertical (derecha)
+vim.keymap.set("n", "<leader>tv", function()
+  Snacks.terminal(nil, { win = { position = "right", width = 0.4 } })
+end, { desc = "Terminal Vertical (Derecha)" })
+
+vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { desc = "Enter Normal Mode" })
+
+-- Reiniciar LSP
+vim.keymap.set("n", "<leader>c.", "<cmd>LspForceRestart<CR>", { desc = "Reiniciar LSP (Force)" })
--- nvim/init.lua
+++ nvim/init.lua
@@ -13,5 +13,21 @@ require("autocommands.php_template")
 vim.opt.conceallevel = 2
 vim.opt.concealcursor = "nc"
 
+vim.api.nvim_create_user_command("LspForceRestart", function()
+  local clients = vim.lsp.get_clients()
+  if #clients == 0 then
+    vim.notify("No active LSP clients to restart", vim.log.levels.WARN)
+    return
+  end
+
+  vim.lsp.stop_client(clients, true)
+
+  -- Wait briefly for clients to stop, then reload the buffer
+  vim.defer_fn(function()
+    vim.cmd("edit")
+    vim.notify("LSP restarted successfully", vim.log.levels.INFO)
+  end, 500)
+end, { desc = "Force restart all active LSP clients" })
+
 -- vim.opt.spell = true
 -- vim.opt.spelllang = { "es", "en" }
