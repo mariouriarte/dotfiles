52bc9ca439998c0baadbe612eb596ce60cea698d
Mario Uriarte <mario.uriarte@gmail.com>
2026-05-14 23:01:35 -0400

    Switch Neovim to Sonokai shusia

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,15 +1,12 @@
 return {
-  -- Instalar gruvbox-material
+  -- Instalar sonokai
   {
-    "sainnhe/gruvbox-material",
+    "sainnhe/sonokai",
     priority = 1000,
     config = function()
       vim.o.background = "dark"
-      vim.g.gruvbox_material_foreground = "original"
-      vim.g.gruvbox_material_background = "hard"
-      vim.g.gruvbox_material_enable_italic = 0
-      vim.g.gruvbox_material_better_performance = 1
-      vim.cmd.colorscheme("gruvbox-material")
+      vim.g.sonokai_style = "shusia"
+      vim.cmd.colorscheme("sonokai")
     end,
   },
 
@@ -17,7 +14,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox-material",
+      colorscheme = "sonokai",
     },
   },
 }
